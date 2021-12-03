7c63b2fc8898d1f75b507607ebe5406e4ee13fe3
Thiago <thiagodaqui@gmail.com>
2021-12-03 08:04:12 -0300

    commit inicial

---
+++ src/test/java/com/github/thiago1bd2/citiesapi/CitiesApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.thiago1bd2.citiesapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CitiesApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
